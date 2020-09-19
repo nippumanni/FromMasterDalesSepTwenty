ce5f71fd72e0a25a19670c62874b36b454f7416c
Dalesh Prashar <prashdale@gmail.com>
2020-09-19 09:48:08 -0500

    Whole set commit

--- app/src/main/java/com/example/daleshprashar/daleprasseptwenty/MainActivity.kt
+++ app/src/main/java/com/example/daleshprashar/daleprasseptwenty/MainActivity.kt
@@ -1,24 +1,43 @@
 package com.example.daleshprashar.daleprasseptwenty
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.ListView
 
 import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.Toast
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import android.graphics.Movie
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import io.reactivex.Observable
+import io.reactivex.Observer
+
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: CustomAdapter
+    private lateinit var  photoList:ArrayList<RetroPhoto>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-
+        recyclerView = findViewById<View>(R.id.list) as androidx.recyclerview.widget.RecyclerView
+        photoList = ArrayList<RetroPhoto>()
+        adapter = CustomAdapter(this, photoList)
+        serverCall()
+        recyclerView.setAdapter(adapter)
+   //     setSupportActionBar(toolbar)
+/*
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+        */
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -36,4 +55,56 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+    fun serverCall() {
+        /*Create handle for the RetrofitInstance interface*/
+        val service = RetrofitClientInstance().getRetrofitInstance().create(DataService::class.java!!)
+      //  val call = service.getAllPhotos()
+      //  val secondCallString = service.getDummy()
+        val postBodyString = service.postBodyString("requestedData")
+     //   val passANameParameter = service.getCommitsByName("stringPassed")
+        postBodyString.enqueue(object :Callback<Object>
+        {
+            override fun onFailure(call: Call<Object>?, t: Throwable?) {
+                t.toString()
+            }
+
+            override fun onResponse(call: Call<Object>?, response: Response<Object>?) {
+                response?.body()
+            }
+
+        })
+
+        val retroCallObj = service.getCallRxJava()
+        retroCallObj.enqueue(object : Observable<RetroPhoto>()
+        {
+            override fun subscribeActual(observer: Observer<in RetroPhoto>?) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+        /*
+        call.enqueue(object : Callback<List<RetroPhoto>> {
+            override fun onFailure(call: Call<List<RetroPhoto>>?, t: Throwable?) {
+            t.toString()
+            }
+
+            override fun onResponse(call: Call<List<RetroPhoto>>?, response: Response<List<RetroPhoto>>?) {
+                response?.body()
+              //  Log.("a",response.body.toString())
+            }
+        })
+        */
+    }
+
 }
+
+
+
+
+
+
+
+
+
